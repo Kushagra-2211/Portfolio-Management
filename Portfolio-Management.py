e315446a7fee9c2dbda41e835b80053b0f5adad0
Kushagra Chourasia <kush@Kushagras-Laptop.local>
2026-02-23 20:23:41 +0000

    Initial commit - Portfolio Management project

---
+++ Portfolio-Management.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 23 19:42:12 2026
+
+@author: kush
+"""
+
+import pandas as pd
+import numpy as np
+import datetime as dt
+import random
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+import plotly.io as pio
+import os
+pio.renderers.default = 'browser'
+#%%
